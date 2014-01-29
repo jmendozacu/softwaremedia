be51522f52b9331ab57af4a7562e099d21644ff3
David Landes <david@landesapps.com>
2014-01-28 17:54:43 -0700

    Updated the crawlers

--- shell/crawler.php
+++ shell/crawler.php
@@ -1,9 +1,11 @@
 <?php
 
+echo 'Start: ' . date('Y-m-d H:i:s') . PHP_EOL;
+
 require '../app/Mage.php';
 
 Mage::app('admin')->setUseSessionInUrl(false);
 
 Mage::getModel('enterprise_pagecache/crawler')->crawl();
 
-echo 'crawled' . PHP_EOL;
+echo 'End: ' . date('Y-m-d H:i:s') . PHP_EOL;
