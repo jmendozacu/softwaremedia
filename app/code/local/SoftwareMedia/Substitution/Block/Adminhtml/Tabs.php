e76cbff4c9726e6180783d3aaa8aa9ace6fb74dc
Akasirus <jlosee@softwaremedia.com>
2014-02-06 14:55:21 -0700

    Fixed lots of stuff, overide products tab, etc

--- app/code/local/SoftwareMedia/Substitution/Block/Adminhtml/Tabs.php
+++ app/code/local/SoftwareMedia/Substitution/Block/Adminhtml/Tabs.php
@@ -1,5 +1,11 @@
 <?php
- 
+/**
+ * Catalog product tabs controller
+ *
+ * @category   SoftwareMedia
+ * @package    SoftwareMedia_Substition
+ * @author     Jeff Losee
+ */
 class SoftwareMedia_Substitution_Block_Adminhtml_Tabs extends Mage_Adminhtml_Block_Catalog_Product_Edit_Tabs
 {
     private $parent;
@@ -10,10 +16,10 @@ class SoftwareMedia_Substitution_Block_Adminhtml_Tabs extends Mage_Adminhtml_Blo
         $this->parent = parent::_prepareLayout();
         //add new tab
         $this->addTab('substitution', array(
-                     'label'     => Mage::helper('catalog')->__('Substitution'),
-                     'content'   => $this->getLayout()
-             ->getBlock('subblock')->toHtml(),
-        ));
+                'label'     => Mage::helper('catalog')->__('Substitutions'),
+                'url'       => $this->getUrl('*/*/substitution', array('_current' => true)),
+                'class'     => 'ajax',
+            ));
         return $this->parent;
     }
 }
