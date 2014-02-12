00ff4bafebba3df078452b961b5373a86a6393b7
Chenguang <zeiguang@gmail.com>
2014-02-12 11:31:36 -0700

    fixed a bug

--- app/code/local/OCM/Quotedispatch/sql/ocm_quotedispatch_setup/mysql4-upgrade-0.1.3-0.1.4.php
+++ app/code/local/OCM/Quotedispatch/sql/ocm_quotedispatch_setup/mysql4-upgrade-0.1.3-0.1.4.php
@@ -6,7 +6,7 @@ $installer->startSetup();
 
 $installer->run("
 
-ALTER TABLE {$this->getTable('ocm_quotedispatch_note')} ADD COLUMN `created_date` TIMESTAMP NULL DEFAULT `CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP`  AFTER `created_by` ;
+ALTER TABLE {$this->getTable('ocm_quotedispatch_note')} ADD COLUMN `created_date` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP  AFTER `created_by` ;
 
 ");
 
