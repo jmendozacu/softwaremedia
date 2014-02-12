1d52b43f203b7733c395cf0a4bfd187f584cc043
Chenguang <zeiguang@gmail.com>
2014-02-12 11:07:15 -0700

    fixed

--- app/code/local/OCM/Quotedispatch/Block/Adminhtml/Quotedispatch/Edit/Tab/Form.php
+++ app/code/local/OCM/Quotedispatch/Block/Adminhtml/Quotedispatch/Edit/Tab/Form.php
@@ -49,6 +49,7 @@ class OCM_Quotedispatch_Block_Adminhtml_Quotedispatch_Edit_Tab_Form extends Mage
           'label'     => Mage::helper('quotedispatch')->__('Notes From Customer'),
           'class'     => '',
           'required'  => false,
+          'readonly'  => true,
           'name'      => 'notes',
       ));
 
@@ -88,10 +89,19 @@ class OCM_Quotedispatch_Block_Adminhtml_Quotedispatch_Edit_Tab_Form extends Mage
       if ( Mage::getSingleton('adminhtml/session')->getQuotedispatchData() )
       {
           $form->setValues(Mage::getSingleton('adminhtml/session')->getQuotedispatchData());
+          
           Mage::getSingleton('adminhtml/session')->setQuotedispatchData(null);
       } elseif ( Mage::registry('quotedispatch_data') ) {
           $form->setValues(Mage::registry('quotedispatch_data')->getData());
       }
+      if (!Mage::registry('quotedispatch_data')->getExpireTime()) {
+      $now = new DateTime('now', new DateTimeZone('America/Denver'));
+      $now->add(new DateInterval('P1M'));
+      $expire_time = $now->format('Y-m-d H:i:s');
+      $form->setValues(array('expire_time'=>$expire_time));
+      }
+      //die(var_dump($form->setValues(Mage::getSingleton('adminhtml/session')->getQuotedispatchData())));
+      
       return parent::_prepareForm();
   }
 }
