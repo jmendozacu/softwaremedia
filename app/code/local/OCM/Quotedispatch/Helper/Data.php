1d645a0084feb4bb0248928b2eca18457a5ef067
Chenguang <zeiguang@gmail.com>
2014-02-14 12:58:27 -0700

    fixed

--- app/code/local/OCM/Quotedispatch/Helper/Data.php
+++ app/code/local/OCM/Quotedispatch/Helper/Data.php
@@ -97,7 +97,6 @@ class OCM_Quotedispatch_Helper_Data extends Mage_Core_Helper_Abstract
         //die(var_dump($adminLastName,$adminFirstName));
          try {
             $formatted = $mail->getProcessedTemplate($variables);
-            die(print_r($formatted));
             $model->setContent($formatted);
             $model->setQuotedispatchId($object->getId());
             $model->setCreatedBy($sender_name);
