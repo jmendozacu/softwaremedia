3214c8bb6d9a5b3f1baa72b4f97f49e7a770ac5a
Akasirus <jlosee@softwaremedia.com>
2014-03-04 10:29:00 -0700

    Added open source version of quote module

--- app/code/community/Ophirah/Crmaddon/Helper/Data.php
+++ app/code/community/Ophirah/Crmaddon/Helper/Data.php
@@ -1,113 +1,190 @@
-<?php //00453
-// Cart2Quote is a commercial software module for Magento.
-// Unpaid usage of our licensed functionalities is prohibited.
-// See www.cart2quote.com for more details.
-if(!extension_loaded('ionCube Loader')){$__oc=strtolower(substr(php_uname(),0,3));$__ln='ioncube_loader_'.$__oc.'_'.substr(phpversion(),0,3).(($__oc=='win')?'.dll':'.so');@dl($__ln);if(function_exists('_il_exec')){return _il_exec();}$__ln='/ioncube/'.$__ln;$__oid=$__id=realpath(ini_get('extension_dir'));$__here=dirname(__FILE__);if(strlen($__id)>1&&$__id[1]==':'){$__id=str_replace('\\','/',substr($__id,2));$__here=str_replace('\\','/',substr($__here,2));}$__rd=str_repeat('/..',substr_count($__id,'/')).$__here.'/';$__i=strlen($__rd);while($__i--){if($__rd[$__i]=='/'){$__lp=substr($__rd,0,$__i).$__ln;if(file_exists($__oid.$__lp)){$__ln=$__lp;break;}}}@dl($__ln);}else{die('The file '.__FILE__." is corrupted.\n");}if(function_exists('_il_exec')){return _il_exec();}echo('Site error: the file <b>'.__FILE__.'</b> requires the ionCube PHP Loader '.basename($__ln).' to be installed by the site administrator.');exit(199);
-?>
-4+oV5ASdX2LPktmACESH1bVlN6VrfchzBQHwdEq/+SDjBHkFKiQkqWFv5D5Jyidbw4MBoXsqL5Nc
-5TEIz53wlWZ1OaOArZLudHIlaWZqVugPcHHxxi73utcKKmdadcm1xkg2wZwnrSTEgmfdc6/99i2S
-VPZAB2N/rKepUrtSsZ9MJPhGhWVY8kFCZdpPxgY7jXYPNRJ8kKeYRRlHRkUCHcpOMpq0yYjaRv22
-xexFnc9GR6WsOOwBf4lyctK2KkXRRGiGGii6tFHnDWuoP/NCO6AHuproCMLM785eJevacUluErS8
-i/dPEhr2A0V/xS/A1d6AuH7JBMlfy2aL7sLL74hr7klfkeJf0fVEFpMoYW6YsHeAMsA/+zamYecf
-JhShH4gLVA77vUBSElOUSyziWM/MU2C3UqcMqZ2jFMzYotD/MquXglwdZ6SFMnDuDJwPdDd72S5h
-IOlb+8ijX/y/E9Y5SWQ38C86bf1mZiSxSBW1WkIMl10PNtOhPOj4SQ1gBOlBuHKgfSZiq5RfL46z
-ut+wiCyMATvlkLEe6BMLiUCtLY3rhmR3tjDndDhg1K4QkDAYopjewm9JWEK8knnB6HziL6gbTZQF
-kIFpuyJFAwDeUdYmUu4zGiqmlLg/4laH/ujv0FT2LLHXTynejE06NOUeVJNFNcNzfQ6doxyDcMQP
-WaJf4FQbTS1g3WP8cg8S682wopMCMvllPKdWGcioisNIXMhdoNU+eJ5KjMD9AiiusJ0pTf4OAaHl
-G+sJ33eJFK7R7WQpr6HF4NU3k2F/BH3bVZ4KBwWd55Ef2LhD2oUGh/5zQS8FzrSSqX7BeO7LwYQH
-gHweWv9KiSRCCGinp43VwItvt6FKMfuiTlfq2cZ7rCj9RTmQqnupNGsNpU+tHepZAWHYEKQIyZLT
-I0Jj+B3qNUcvr8jEiMXrM9cT0B+s6W/pj/2WFv94d945sw1Z9J0sGD6B79cmEYsq0fNTc5//sn8s
-050lPSsGBuEX7cy5pm6qEm8FwRL7tW7u7NEB1gdc4QxN4iXkoR57VeIlvEgjDQeqmQSuAfk+UENg
-EzZA7j7CzDow8Z11VLlk4bVZGc8cR2Feqrk9CE8QNjZd0Q4uI2kZNiStz+9i7JjBsnHplpsKtRp9
-FbxLCImWbW2dsaNquwp2KjO4i+K5Ey8vObDMo0KBCHH1Sk+Q/ghQqPuKudC62P5zmZfpUxWEFIcc
-26My+GFlv7Mz+rYeBM6YWqPPrtmVgZVpG0SP8lLeJJfuC1ECzM/JuHbP8Fs/L90oyzdWIthSr3ah
-+19IRtghrMGDGEM1uMTPm6bzwqNBdmX0U68cQa0XAQiP40BVj3ff1RUwO8ArslVm38MYGwbK9vsV
-Gme5wNNsWprdXOLl+r+eKrkmJK7CHx/quLpgPEiaATDDk6CcbamVpmB7b8EiWnTMYo70J2focEW5
-y8Y6wM5wuWhqo9WVQvnCVhqS3TMoaxzBr1epEmV9u4Ropl8jvCfxhdkE86QGHFKaSDYgO/rV4vZW
-MJt+GQ2qBzLMZ04wTnWlwlOo3uoxuBSZtT0fiZ8gblHmBLrFt1N0xhpN/fZafe81alnCFw/ciL4r
-xneHGsflIWjibfX46m5P9CmChwSlpeQaHThUrHHC1NufaO3kxI/krQ+NS3AtReldln8dUaWhqS0b
-/un6oQX8YYkn16JLRbqqrdol1DzziUV2QO0hZOfGil+5OMYB7DlJP/m412Tbng3SUr64Pzd1QGQ3
-ax9YJw6dNExh4SiqOjaEtY4PrjF31iz5iwhZ8SgY8NiM67riqS3vMD1VzCl5tufXwjqesyU/zL89
-5w8RU0nOaLxG7NMBVzp3FtBwYgBoKf5RVE1js7TV1Wllu/iQVR+KYIYf8/SmaD8+K9ZOWWyReK7k
-Lyn17CXRi6vSpGGIN4kIzmWxFktKZYFxSqZhJIlZ86ATjRWs4rGsErB3J15lajVBGYkRBoYzql1L
-3ANpGRmG1L2zJZSg1ujhiu5LnvfGoWlLRkv1N6yl80cyX+KUpL7jsTBMDsLDUw2zsxGW1iYMciKS
-5yroz5bIEK4up+KXtbj6d1a9ESMFNaY1VSTRvk9uG7vO4DTHTVbfwfu7PVQU/BbjnSr7YXj46oll
-NYJ7mDMRlEM776oeZPxyK+kiU/1RaLVcgvj7CNo8SREK2EYmrVpBdkqAjrUwg/JnNL0T2RqaPnXE
-VQVu/ZVil6d+c2dLqRYkWdh77nbw4WN2XslBcMLhwlLZR4hy7nkcY6i/JO1rR/Jq7oTeYgDlU85J
-VMKfoynaswBPCpV4hJ0UbQQ5nIn4L0MqPJP5hiz6J78+tlm/mY6WROiT8AevomDCvXwLWgCFC8jl
-kCHgMBvs0DQrIIx9LWijoGuUoBdc5x8smoEFwbZvrHvSbHi0PA3blFeDmcywBLovz5wz1QM34Mla
-cMt/MEEj8pg9pTWSD7e6KCN7ksFJH4Mq5mvAqyl0B7RjODXrfNJ40oENRoveROI9NVUBGwERbJjc
-PN8jKoLw1E14EYGZ+DvLLsOHVCjW35QhxKaeUrjlcrWdDzP9tkuJFH1t5aPuVOBT+ZGLVoIzntar
-J3+1u0oIzErBbE36c5VZ6GUtrc88H1+GKkvNE3CT3kho1WcT4tBmDJ1md5aJzEOfVwiJWC5BA973
-EcKAG2Y4wX8B9l7GNxuZ5mj5taXBkdYWSsUhcBVpm2UPuz6oWKL5Q+sL5FPjn6L++VLFz0G6mgZW
-i5y6dBcoNIzS9gBousU0E0FTcE1sYgl2Jni38x9oQxqMifSLN9NVKR/DzrTFiN+HJxRhfM6+63xr
-z8qlYMUPhfMCFasvfhLiPUPLbLye3fbIEBZXnSFBgQ2AXOmWLGpaN+pcBz5wIsxEcfvGaZhlziE1
-tWJfRkltpWy+DRdG1QVgFs81ZRmu1tjgFXpTLJEYEEgzAH6qOSzPXxdn16iLZ4XHNHSZOM0/0DHE
-v6WpZr2/YEg0Spqzhyez0kKzx0mxMti6OdxCEty5pdaOLpbWs9ycHi+Pt1+/0lENQQC/5Ap4nvsv
-2Gyge/2jemS1xfU3zj++IpB/Q+WRWh5nAM0fA3To7NfttRaY4zaZNzLHI7tsAcEbARIqPEgPikYR
-s4pkpBItg2lSE/JEsgrbt/Iic3Xf3JFiZLCdL9e9fL71BvbwoswaYA8oH9IuslfcaYOf/lXTISiI
-/SaRuU/kYw9qTSwjGTBKl8MYCtbRFM4148mU18Vl1NQUnTHuiRVqTow1Qyop4dMsqTVrW0K9mhvs
-vvnUBqg5/ZJfr7BkHTnZidV/il4Dhjpfr5JTBu/3vO0xWcFXT0ErSGIW+sqbJuAGd+rcRq4bNjIq
-Rcq9KfJhNUFYf2dh0yZJUp1WN13KCkuKGISC6oiZZy1Mbv9bqwYwLRLoZROMLexMbh0XRkFJmu2n
-D1dXxT4rDzQN1z2qxWK4IiOgj5PGGNbNMTnI9Z3THHrap9CNI9jRaqdHA5C+TExqp7x7tv/MxxsP
-7hka/Mhq1RW0dyxYppFXr0EiJL9hb/n69MSDaOI9KgiTHyY9cGwcp/jIIyo8MpAU6bsFlgjkLJaT
-/51OKOMsW4GXkCXZTbLZ3CfBbZyCS4LITuEmo+dvSZJ+qKz0nOVYAzcyRNTLoOGa9PVqxFDjYQra
-CtJz5Rv+JFbZS384y+0RDEMs1sH2hyWeLj7Zjh+q+grsMWrbWxCLhBBq7r12Tc35n2WQyyIK1MrD
-08kJT2Mz0iiCtqXo2vWKTSmBUVnP//xADlW4mzrhQ4H+Gm3BNJ5spzwtd+do462FUPx2t5S0cOK9
-v5jKBtFF9MwcP5uOW1THxy1b3rpmdjLGNIy4bDdu/J2H7LsJu4U2GnUQlh0h11K5jqM2/A7PihUF
-fYuBULfgsbzKPJgIyp4r0q6Nn54bHzWgwtXhYjy5PP6VrjIunfry3hObB1DMIQL8UbH1o4nykJNl
-aryGhvQVaBZ9a7shqW0CjHJRsBdd8A2x1MDp0NB3VcbzDED7luoXEl/7mDFCuRN2MR/8ISiNOuqh
-5aTr5aMvTxwzdYbJ4RjhbNsy26c3mf3gJH0soymTxZyapJODpF85c/VZtzhQzAdxot3RGStBlazu
-obBgYidfg8rwiqk7VDfxnAQY7bSYpvspLpxqUOUpYtSAu+MvY/z2Gg5K9nblI/Gz/cqv5PKNXTfs
-QAVrSGFyrC9/5G+dD5cXqJ25XiPhUYWu955E5vzM/ljYz1rXqScDI8Q2eqO8WdeJbG9eDWyFSkCJ
-VHN7r8LSrP7joZzMsRLzmjpAv4WsTG5TuqDdNvZiRaRZJFnpwF/100tPpUvoJEiZkBu/9BV0C2z6
-yGlmgWO8nHdcMN6TlpAHHqNEEFfVepf4rMynwxWtEFbPwp5ZuAskTBfha6WA8/sU+mGwtAjFkvX0
-jp6SZeF/kXuYRZeZzpedAOa9IKA+fsh/QANAGIQ7PSGEux+zkCZYOGR2ZB2Gn4nK/SWUbZ/hufYX
-7SkmyVNpVQP3fsy0BJlWTr3ni08cLvBu7caZkI8nJ/UzFgFtad/p+2YGtUzl+2ppSpxl0vDo+KEX
-7TC/UTyxgCkKmWmEweghM6WBZ/U6cX+HOBZVuWGaChkW4bH0nw6JP2/Bty3fzKk5D62QRRV+byjO
-T+f4VwTXPDiIXQVnQcPmThgvS1c8eLnPRc4/g3/fU58ow9lWo02DXEKw8bNFZ99JXIZ0rKBAKlIz
-CFpW0En30NcFVfll7HRpXz3L/0LbkkC4cDxemo2+v9F0o3DfC6IlCbsJNZ7nB7KiAMAt1Uz4Wx15
-bPt6+gql7hl6hXETXCsaRaoOzHFaQu4Usbim71UjPIWubHKhy6/rjO8RmeqFXzPahxDg50yUXFwF
-KELfoXtXwUL2taQOYqHtkXp0ZeegVPcZcw7RcHT1ijpnb/KJKS8Hu1h6ECuk0GSNBiKL7gdnRrrA
-sKJa4QoDHzxvjGnqTduG/uSYg0nUbUEWVKuNTr+ef37giAkGXIeqQGTQPh9KI6Rsy9YtzFmGQI6O
-fh1SEsjLWLfrdoT27sy2x5CM627iQ96ZUBIZQE98v7FAFtWS4ByGNbhgsVpsImHejoFS5e63ftdG
-TojC7g+1qjTTGuUyUSVuSHOuZIZSD2tqaHWsOccLrLGFQ9uYk5IsTcbuhOgICkURbESDZr9QYmfS
-L+ZRHIX7j+RMqQpyPJNNV59e5Or0tyFkptqU2uAzQk3LOOy3Gr4XIaUXnhelL93oJf2uOXijELiF
-cCWwl3hGvokXr5dh8kz59Hs5vIKsQ9cfIP1/V7hpIXS4btYu64dhtmguEjC8S+C89qAvZhFXhog3
-cESjstWfsr7aXZswjO1xijHZ5G7J+c90ZVyHN+aI9jmEvGkGedJUHWzBFXIxVDtwIrSXzl5y+F5U
-En/gSHmWOz6O3pe97EQ5lB6/Ibqfmn7V01n/UAojGTtnMkgAerFcSbgUpMxhfNGhMipPPy7STj57
-dNe7U7K8FVZ5Al+2x+BKx1nRQAW5dZY7GdzukES+IeFObAWdvex96aZymXViNCg6XQ15H3W5ybOj
-Pr+EQQ0LeGrbsmXTUOmwGm393gszfhSHWldGMUjSYNH8az1CNk45CAtbIrrTAT7241cYaoPXJq2L
-OPtJelrcIXaS8ts5W+KV98++YtHN7E2uDcdxQchKsatAbzyLOhFIyxX2otfrwC3Y9G6PbLDlqAUp
-dy/bvNPoKCtl2hfaltIBCzYVvjVok2DHX9GoCO4Knk39K3Igqrkkp9+06uBrZoHM8jfTpd5XQ0zH
-drKXrUhKSsC7XvD4xBgG1gkuQCvIMj7A5SVSfPLmXGeA5G7O10PSAzV/cMe8bqhwjj/ZdJznVB3Y
-H+hbECSTGEUh3E8+kcSAgaCVjcY/w53j8dcSwnZJvPnM0qGSd7NUXW6vtWeJbM5uPGrdK6NxUpF0
-LJscRCrwQQAuKtp/UIqaa9yWsfBCSfygEARYpbQ09M5ZDkx2XlA++7jGgHME28KVSRbWXME2/zuA
-OzDV779VK3NBk97U2EzDtrbe2L1Hz15xVBMOUlRwOnyuogBCJaFiJvAfrE1EzTxeXqiOpLxSY0Sm
-6BnfdJrGT/AfB1+IMrHNhPQmw2fuv+P5fS8dlGATfzVGhsagi1LwErITW+JJ41DQqcQ9d5aCaE7l
-DTK9YdwOLACOLB1IfpvVl3OIRZU+5A5ucq5A22mbb+d25gJxx5tCAmD43CFK3AWWtAVCCMT3MuFC
-ITWuq5pkKul6PTbogJGfBNWuh2nL0satXfs/09q1Ck1E5b9jP+zw/FWOlPeBss3g0UXnzPoUNLAV
-AWE+0vonsMENRtmL2CF1XaCuovgl7kck4SxqR+4z2e6wfLwdNzH+UlteZR0ewGrC5SYs53WI8ks+
-8qyVnmyb4J9b6T7jnTHLloTN6O/nUtOTZ3VtsD84VSc4vkEacQns+YKXdXA0x1tPBmUhGZKR21wN
-yamb6VQJz8A8SYn7fGX3ChnfGZUlnmKnuQq23L0dowfVCc+QnKV/ZCOaWEjbUc/a/P9hEmbu/VnW
-VtcGZSI4xErGWRt+IJG4uuDWnhdHlwF2elOCuyGNVT/vqI+yY9j0L1JDeFEsoSDz+SJREWhCuHtL
-+LoNxvJCLeHQTFTh6Mf0XhboFGXm/1r/JqtKhHlhPymw1t3LwgnfZcSIgjUQbpDziPBN+7a0Txfg
-Qb+pxnjb8FxOQuTKgipCUwpKnwkC6zRU/k8Q7k0hAnllNKQ/+9jQJJ0U9Sfl9bRw5DAnLk//uL8S
-d5vEML9Hdu3bS0YWr9QCEzHYfzUi0FEv0JFu203hZG0tx0Z8B0CsLg1uoXeBDx5PYXh5prOW61eK
-b5AJC0aH6V9ul29iKnc6WWcffOfuV9T+/uAeaPdtXKZqD/XBsoKx668YKYc7OBu9rGINoiMQWLFW
-NMFNpPCpfKM6TxDKEKMvI/BTLRZsv467UDqoUy9GR6Ncdu8Bu1xVgWNKKu24dwskpbRI/5RlttUe
-dw+qdC8e/EYySKUgYIg8oIWe8ynPOC5ky+wrQM3To2jjx1frWFaIocwrZw2AxomoScC2IgiuKOvv
-Eihkt8e59dBMUXGfQ29Vd7oMcoh2aypbGd6P1hhvnz+JNKE2EI1j9A7PwhFA6XinloOEibLydoAZ
-EyuuG+qgJmmUX5rV0wvlcocs1HqMYzB7Dsil7dyPmiuzsfq+wSZe1GmNmQHouUA6jIT7ooS4WFlo
-YOVgLYLQ55Qpwmcv8w0LN3hUuS2PQUQsZfcZxIQVTfdjcDNc/hT1WmQ1blSEr885bGDOZXp81iN0
-KtBdvgaEyeX7KzY4UwocY0Yk4hgpGlIS+a6TQR/OQpA/Y/BTJZtmtl6udNYAnxneyzJnq7xEUvRb
-lohzpPxXYiyQq+i8eUbTPgKes6cPsAGJ3e58EEkIVZX7/oq6ilVqtaswHxkfC+4aDiU7FJfwEVIR
-QdlIvDgAtsNqunshavHgFGuRFt3pVL7JUs9evIxS8P3bcRSqkszq+lNVN//YXikvlNCGTOkRYFpM
-8bMQX8jLH+DL66c2SNbMh5/3mysADQBpywauJeiqM2+bNxeMhbPfgyseX7A8pmj/UfJLzkrmBD9V
-MoRyq5N33d+1ZSzmcPbhR2LPP8fzQPY2BaCe6AAtDCc6VREb5p21SXTDFY5DPmaVsbfnjnrsYsh7
-zLTmK/of71/6XO8TjL3IDJWe6tkrWT0XwvL1jOEMEGui2lwuZWPa4j+WedjcVhsYNZQF6sIU30A+
-lOHyC22JxxcuPNtjgarQ+s3dBZtyTS320lCJ2mwWPrQiiy4ILv1DLbUNAAiKeH0Av4XDE3PROLHy
-p0I/4Ul06zthIndSzCouYo3rcc94Y4+9mLCiQGRd5eulfbo99BuIwBNz7LoDS3M+cii3GxMwLtvs
-19lZB9J7D7F161ItRxH/bC2gh9e+phWXeK01/PhezZgvIQRxdfP6eJIDaPfqYiL1XvilY+BYEs4Z
-gSHMjpNwLEYSg4XG5fQaezAvzdpgkyvBo8J3HIRBBBz2B29UFUxy+8mjMnPIWszh4im9itsaBLNn
-bKm2bBQCUSMhIn1c/a4eQggkDHr81OMB+Pm/At+I3SYC9RIQlCnR6GUKKFr3Qqio7t+hs78aP0==
+<?php
+/*
+ * Cart2Quote CRM addon module
+ * 
+ * This addon module needs Cart2Quote
+ * To be installed and configured proparly
+ * 
+ */
+
+class Ophirah_Crmaddon_Helper_Data extends Mage_Core_Helper_Abstract
+{
+    // Defining constants
+    CONST CRMADDON_NOTICE           = "The CRM module is part of the Cart2Quote Enterprise version. <a href='%s' target='blank'>Upgrade or change to a paid plan</a> to unlock more features";
+    CONST CRMADDON_NOTICE_LINK      = "http://www.cart2quote.com/magento-quotation-module-pricing.html?utm_source=clientwebsite&utm_medium=clientwebsite&utm_term=upgradeCRM&utm_content=upgradeCRM&utm_campaign=upgradeCRM";
+    CONST CRMADDON_UPGRADE_LINK     = "http://www.cart2quote.com/magento-quotation-module-pricing/magento-cart2quote-enterprise.html?utm_source=clientwebsite&utm_medium=clientwebsite&utm_term=upgradeCRM&utm_content=upgradeCRM&utm_campaign=upgradeCRM";
+    CONST CRMADDON_UPGRADE_MESSAGE  = "To use the CRM module of Cart2Quote and send messages to your customers <a href='%s' target='blank'>upgrade</a> to Cart2Quote Enterprise";
+    
+    /*
+     *  Retrieve array with message templates
+     *  from crmaddontemplates table
+     * 
+     *  @return Array()     // Database data
+     */
+    public function getTemplates()
+    {        
+        $templates  = array();
+        $default    = array();
+        $DB_templates = Mage::getModel('crmaddon/crmaddontemplates')
+                        ->getCollection()
+                        ->addFieldToFilter('status', 1);       
+        
+        foreach($DB_templates as $DB_template){
+            if($DB_template->getData('default') == 1){
+                $default['default'] = $DB_template->getData();
+            }
+            
+            $templates[$DB_template->getTemplateId()] = $DB_template->getData();
+
+        }
+        
+        $templates = $default + $templates;      
+        
+        return $templates;
+    }
+    
+    public Function createOptions($templates)
+    {
+        // create options from template array
+        $options    = array();
+        foreach($templates as $key=>$value):
+            if($key != $templates['default']['template_id']){
+                $options[$value['template_id']] = $value['name'];
+            }
+        endforeach;
+        
+        return $options;
+    }
+    
+     /*
+     *  Retrieve array with messages
+     *  from crmaddonmesages table
+     * 
+      * @param  decimal     // Quote Id
+     *  @return Array()     // Database data
+     */
+    public function getMessages($quote_id)
+    {
+        $messages = array();
+        
+        $DB_messages = Mage::getModel('crmaddon/crmaddonmessages')
+                        ->getCollection()
+                        ->addFieldToFilter('quote_id', $quote_id)
+                        ->setOrder('message_id', 'DESC');       
+        
+        foreach($DB_messages as $DB_message){
+            // replace message tekst with Html stripped tekst
+            $newMsg = $this->stripHtmlTags($DB_message->getData('message'));
+            // get short message
+            $shortMsg = $this->getShortMsg($newMsg);
+            
+            $DB_message->setData('message', $newMsg );
+            $DB_message->setData('message_1', $shortMsg['message_1'] );
+            $DB_message->setData('message_2', $shortMsg['message_2'] );
+            
+            $messages[] = $DB_message->getData();
+            
+        }
+        
+        // create empty message if no messages are found
+        if(empty($messages)){
+            $messages[] = array('message'       => ' ',
+                                'subject'       => ' ',                        
+                                'created_at'    => NOW()
+                                );
+        }
+        
+        return $messages;
+    }
+    
+    /**
+     *  Strip Html markup, and
+     *  insert break tags
+     * 
+     *  @param  string     // message text
+     *  @return string     // new string
+     */
+    public function stripHtmlTags($message)
+    {
+        // Defining Arrays for replacement
+        // first    - array with target text
+        // second   - temp replacement text
+        // third    - final replacement text, if different from first
+        $totalReplace   = array();
+        $totalReplace[] = array(array("<br>", "<br/>", "<br />"), "[[!!##BREAK##!!]]", "<br />" );
+        $totalReplace[] = array(array("<p>"), "[[!!##POPEN##!!]]");
+        $totalReplace[] = array(array("</p>"), "[[!!##PCLOSE##!!]]");
+
+        // trim and decode message
+        $trimMessage    = (trim(html_entity_decode($message)));
+        // replace text with temp text and strip Html
+        foreach($totalReplace as $replacetext){
+            $replaceMsg     = str_replace($replacetext[0], $replacetext[1], $trimMessage);         
+            $trimMessage    = $replaceMsg;
+        }
+        // strip remaining tags
+        $replaceMsg = strip_tags($replaceMsg);
+        // replace temptext with html tag        
+        foreach($totalReplace as $replacetext){
+            if(empty($replacetext[2])){$replacetext[2] = $replacetext[0][0];}
+            $returnMsg  = str_replace($replacetext[1], $replacetext[2], $replaceMsg);
+            $replaceMsg = $returnMsg;
+        }
+        
+        return $returnMsg;
+    }
+        
+    /**
+     *  Cut message in 2 pieces
+     *  for shorttext display
+     * 
+     *  @param  string     // message text
+     *  @return array()    // array with short message
+     */ 
+    public function getShortMsg($message)
+    {
+        $shortMsglength = Mage::getStoreConfig('crmaddon/emails/crmaddon_shortmsg');
+
+        $return = array();
+        if(strlen($message) >= $shortMsglength ){
+            $return['message_1'] = trim(substr($message, 0, $shortMsglength));
+            $return['message_2'] = trim(substr($message, $shortMsglength));
+
+        }
+                
+        return $return;
+    }
+    
+    public function tabIsActive()
+    {
+        if(Mage::app()->getRequest()->getParam('crmbodytmpl')){
+            return true;
+        }      
+        return false;
+    }
+    
+    /*
+     * CRMaddon Module is only available
+     * fot Enterprise users of Cart2Quote
+     * 
+     * @param   mixed      // Message to display, with or without link
+     */
+    final function checkLicense($notice = NULL){
+        
+        // Check for a valid Enterprise License
+        if($notice == NULL){
+            $notice['message']  = Ophirah_Crmaddon_Helper_Data::CRMADDON_NOTICE;
+            $notice['link']     = Ophirah_Crmaddon_Helper_Data::CRMADDON_NOTICE_LINK;            
+        }
+        
+        if(!Mage::helper('qquoteadv')->validLicense('CRMaddon')){
+            if(!is_array($notice)){
+                Mage::getSingleton('adminhtml/session')->addNotice(__($notice));
+            }else{
+                Mage::getSingleton('adminhtml/session')->addNotice(__($notice['message'], $notice['link']));                
+            }
+        }
+        
+    }
+    
+}
