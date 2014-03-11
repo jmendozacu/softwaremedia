c831c7e0b5ec57a346e1e59ed6f6ec949586cf58
Akasirus <jlosee@softwaremedia.com>
2014-03-11 11:44:39 -0600

    Fix 4040 reroute

--- app/code/local/OCM/Checkout/controllers/Checkout/OnepageController.php
+++ app/code/local/OCM/Checkout/controllers/Checkout/OnepageController.php
@@ -2,5 +2,49 @@
 require_once('Mage/Checkout/controllers/OnepageController.php');
 class OCM_Checkout_Checkout_OnepageController extends Mage_Checkout_OnepageController
 {
+    /**
+     * Save checkout billing address
+     */
+    public function saveBillingAction()
+    {
+        if ($this->_expireAjax()) {
+            return;
+        }
+        if ($this->getRequest()->isPost()) {
+            $data = $this->getRequest()->getPost('billing', array());
+            $customerAddressId = $this->getRequest()->getPost('billing_address_id', false);
+
+            if (isset($data['email'])) {
+                $data['email'] = trim($data['email']);
+            }
+            $result = $this->getOnepage()->saveBilling($data, $customerAddressId);
+			
+			//Hijack Canadian Orders
+			if ($data['country_id'] == 'CA') {
+				$result['redirect'] = '/quote-thank-you';
+			} elseif (!isset($result['error'])) {
+                if ($this->getOnepage()->getQuote()->isVirtual()) {
+                    $result['goto_section'] = 'payment';
+                    $result['update_section'] = array(
+                        'name' => 'payment-method',
+                        'html' => $this->_getPaymentMethodsHtml()
+                    );
+                } elseif (isset($data['use_for_shipping']) && $data['use_for_shipping'] == 1) {
+                    $result['goto_section'] = 'shipping_method';
+                    $result['update_section'] = array(
+                        'name' => 'shipping-method',
+                        'html' => $this->_getShippingMethodsHtml()
+                    );
+
+                    $result['allow_sections'] = array('shipping');
+                    $result['duplicateBillingInfo'] = 'true';
+                } else {
+                    $result['goto_section'] = 'shipping';
+                }
+            }
+
+            $this->getResponse()->setBody(Mage::helper('core')->jsonEncode($result));
+        }
+    }
 
 }
