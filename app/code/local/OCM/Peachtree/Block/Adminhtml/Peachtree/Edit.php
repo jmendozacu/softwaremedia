b70009f88b3307c2215b3d8e53a22d2184d733d9
David Landes <david@davidlandes.net>
2014-03-12 17:18:30 -0600

    Added the ability to convert orbital to peachtree

--- app/code/local/OCM/Peachtree/Block/Adminhtml/Peachtree/Edit.php
+++ app/code/local/OCM/Peachtree/Block/Adminhtml/Peachtree/Edit.php
@@ -1,30 +1,34 @@
 <?php
 
-class OCM_Peachtree_Block_Adminhtml_Peachtree_Edit extends Mage_Adminhtml_Block_Widget_Form_Container
-{
-    public function __construct()
-    {
-        parent::__construct();
-                 
-        $this->_objectId = 'id';
-        $this->_blockGroup = 'peachtree';
-        $this->_controller = 'adminhtml_peachtree';
-        
-        $this->_updateButton('save', 'label', Mage::helper('peachtree')->__('Export CSV'));
-        $this->_addButton('import', 
-			array('label'     => Mage::helper('catalog')->__('Import CSV'),
-                        'onclick'   => '$(\'edit_form\').writeAttribute(\'action\',\'' . $this->getUrl('*/*/save') . '\'); $(\'edit_form\').submit()',
-                        )
-		);
-        
-        $this->_removeButton('delete');
-        $this->_removeButton('back');
-        $this->_removeButton('reset');
-		
-    }
+class OCM_Peachtree_Block_Adminhtml_Peachtree_Edit extends Mage_Adminhtml_Block_Widget_Form_Container {
 
-    public function getHeaderText()
-    {
-            return Mage::helper('peachtree')->__('Peachtree Export');
-    }
+	public function __construct() {
+		parent::__construct();
+
+		$this->_objectId = 'id';
+		$this->_blockGroup = 'peachtree';
+		$this->_controller = 'adminhtml_peachtree';
+
+		$this->_updateButton('save', 'label', Mage::helper('peachtree')->__('Export CSV'));
+		$this->_addButton('import', array('label' => Mage::helper('catalog')->__('Import CSV'),
+			'onclick' => '$(\'edit_form\').writeAttribute(\'action\',\'' . $this->getUrl('*/*/save') . '\'); $(\'edit_form\').submit()',
+			)
+		);
+
+		$this->_addButton(
+			'Convert Orbital', array(
+			'label' => Mage::helper('catalog')->__('Orbital Converter'),
+			'onclick' => '$(\'edit_form\').writeAttribute(\'action\',\'' . $this->getUrl('*/*/convert') . '\'); $(\'edit_form\').submit()',
+			)
+		);
+
+		$this->_removeButton('delete');
+		$this->_removeButton('back');
+		$this->_removeButton('reset');
+	}
+
+	public function getHeaderText() {
+		return Mage::helper('peachtree')->__('Peachtree Export');
+	}
+
 }
