20bb8900de7f6b3eb64d92f4cc7fbe975c31412e
Akasirus <jlosee@softwaremedia.com>
2014-04-09 14:45:23 -0600

    cc_saved testing

--- app/code/core/Mage/Checkout/controllers/OnepageController.php
+++ app/code/core/Mage/Checkout/controllers/OnepageController.php
@@ -574,8 +574,9 @@ class Mage_Checkout_OnepageController extends Mage_Checkout_Controller_Action
                     | Mage_Payment_Model_Method_Abstract::CHECK_ORDER_TOTAL_MIN_MAX
                     | Mage_Payment_Model_Method_Abstract::CHECK_ZERO_TOTAL;
                 $this->getOnepage()->getQuote()->getPayment()->importData($data);
-                $pay = this->getOnepage()->getQuote()->getPayment();
-                Mage::log($pay['cc_saved'],null,'temp.log');
+                $pay = $this->getOnepage()->getQuote()->getPayment();
+                Mage::log("pay: " . $pay['cc_saved'],null,'temp.log');
+                Mage::log("data: " . $data['cc_saved'],null,'temp.log');
             }
 
             $this->getOnepage()->saveOrder();
