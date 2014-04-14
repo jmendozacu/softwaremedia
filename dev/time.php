2b33771d6edc693cd2f0bd02aa5ba1b843467663
Akasirus <jlosee@softwaremedia.com>
2014-04-14 10:37:51 -0600

    time test

--- dev/time.php
+++ dev/time.php
@@ -1,7 +1,7 @@
 <?php
 
-//require "../app/Mage.php";
-//Mage::app('admin')->setUseSessionInUrl(false);
+require "../app/Mage.php";
+Mage::app('admin')->setUseSessionInUrl(false);
 //$file = fopen(Mage::getBaseDir()."/var/synnex_data/520985.ap","r") or die('could not open file');
 //OCM_Fulfillment_Model_Warehouse_Ingram
 //Mage::getModel('ocm_fulfillment/observer')->updateProductWarehouseData();
