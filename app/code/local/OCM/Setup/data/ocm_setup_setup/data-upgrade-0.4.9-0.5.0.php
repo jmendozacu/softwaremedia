c16d40ab4d753e6a9a212f8b5e8013bf9207c2d0
Akasirus <jlosee@softwaremedia.com>
2014-04-21 15:17:27 -0600

    purchase order fix

--- app/code/local/OCM/Setup/data/ocm_setup_setup/data-upgrade-0.4.9-0.5.0.php
+++ app/code/local/OCM/Setup/data/ocm_setup_setup/data-upgrade-0.4.9-0.5.0.php
@@ -5,7 +5,7 @@
   $installer->startSetup(); 
   
 $installer->run("
-  ALTER TABLE `mage`.`sales_flat_order` ADD COLUMN `purchase_order` VARCHAR(45) NULL DEFAULT NULL;;
+  ALTER TABLE `mage`.`sales_flat_order` ADD COLUMN `purchase_order` VARCHAR(45) NULL DEFAULT NULL;
   ");
 
   $installer->endSetup();
