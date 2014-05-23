0d0242644e1696935a62c6639afbed022d53d515
David Landes <david@davidlandes.net>
2014-05-23 14:21:37 -0600

    Added the ability to send as your own account

--- app/code/community/Aschroder/SMTPPro/controllers/AsyncController.php
+++ app/code/community/Aschroder/SMTPPro/controllers/AsyncController.php
@@ -2,16 +2,16 @@
 
 class Aschroder_SMTPPro_AsyncController extends Mage_Core_Controller_Front_Action {
 
-    public function mailAction() {
-        $queueItemId = $this->getRequest()->getParam('queue_item_id');
-        $queueItem = Mage::getModel('smtppro/queue')->load($queueItemId);
-        $params = json_decode($queueItem->getParams(),true);
+	public function mailAction() {
+		$queueItemId = $this->getRequest()->getParam('queue_item_id');
+		$queueItem = Mage::getModel('smtppro/queue')->load($queueItemId);
+		$params = json_decode($queueItem->getParams(), true);
 
-        if(Mage::helper('smtppro/mail')->sendMailObject($params['mail_object'],$params['website_model_id'])) {
-            $queueItem->delete();
-        } else {
-            $queueItem->setStatus('failed')->save();
-        }
-    }
+		if (Mage::helper('smtppro/mail')->sendMailObject($params['mail_object'], $params['website_model_id'], $params['transport'])) {
+			$queueItem->delete();
+		} else {
+			$queueItem->setStatus('failed')->save();
+		}
+	}
 
 }
