d308b5d95419135592f05272205d140e0b8554dd
David Landes <david@davidlandes.net>
2014-05-27 11:20:51 -0600

    Deleted the old mailchimp code

--- app/code/community/Ebizmarts/AbandonedCart/Block/Email/Order/Items.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class Ebizmarts_AbandonedCart_Block_Email_Order_Items extends Mage_Sales_Block_Items_Abstract
-{
-    public function _construct()
-    {
-        $this->setTemplate('ebizmarts_abandonedcart/email_order_items.phtml');
-    }
-}
