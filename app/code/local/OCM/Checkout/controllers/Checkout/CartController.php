cc5710f72c962c57fb41b855cfd2b727582ec0c2
Akasirus <jlosee@softwaremedia.com>
2014-05-29 12:28:01 -0600

    Add new cart AB testing page

--- app/code/local/OCM/Checkout/controllers/Checkout/CartController.php
+++ app/code/local/OCM/Checkout/controllers/Checkout/CartController.php
@@ -51,7 +51,57 @@ class OCM_Checkout_Checkout_CartController extends Mage_Checkout_CartController
 
         $this->_goBack();
     }
-    
+
+    /**
+     * Shopping cart display action
+     */
+    public function newAction()
+    {
+        $cart = $this->_getCart();
+        if ($cart->getQuote()->getItemsCount()) {
+            $cart->init();
+            $cart->save();
+
+            if (!$this->_getQuote()->validateMinimumAmount()) {
+                $minimumAmount = Mage::app()->getLocale()->currency(Mage::app()->getStore()->getCurrentCurrencyCode())
+                    ->toCurrency(Mage::getStoreConfig('sales/minimum_order/amount'));
+
+                $warning = Mage::getStoreConfig('sales/minimum_order/description')
+                    ? Mage::getStoreConfig('sales/minimum_order/description')
+                    : Mage::helper('checkout')->__('Minimum order amount is %s', $minimumAmount);
+
+                $cart->getCheckoutSession()->addNotice($warning);
+            }
+        }
+
+        // Compose array of messages to add
+        $messages = array();
+        foreach ($cart->getQuote()->getMessages() as $message) {
+            if ($message) {
+                // Escape HTML entities in quote message to prevent XSS
+                $message->setCode(Mage::helper('core')->escapeHtml($message->getCode()));
+                $messages[] = $message;
+            }
+        }
+        $cart->getCheckoutSession()->addUniqueMessages($messages);
+
+        /**
+         * if customer enteres shopping cart we should mark quote
+         * as modified bc he can has checkout page in another window.
+         */
+        $this->_getSession()->setCartWasUpdated(true);
+
+        Varien_Profiler::start(__METHOD__ . 'cart_display');
+        $this
+            ->loadLayout()
+            ->_initLayoutMessages('checkout/session')
+            ->_initLayoutMessages('catalog/session')
+            ->getLayout()->getBlock('head')->setTitle($this->__('Shopping Cart'));
+        $this->renderLayout();
+        Varien_Profiler::stop(__METHOD__ . 'cart_display');
+    }
+
+
         /**
      * Add product to shopping cart action
      *
