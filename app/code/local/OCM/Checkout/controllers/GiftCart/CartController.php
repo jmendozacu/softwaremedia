3633658895a7560749f540528ef8bf9d57530914
David Landes <david@davidlandes.net>
2014-06-17 14:16:52 -0600

    Added the ability to stay open when we have a bad coupon

--- app/code/local/OCM/Checkout/controllers/GiftCart/CartController.php
+++ app/code/local/OCM/Checkout/controllers/GiftCart/CartController.php
@@ -6,36 +6,39 @@ class OCM_Checkout_GiftCart_CartController extends Enterprise_GiftCardAccount_Ca
 
 	public function addAction() {
 		$data = Mage::app()->getRequest()->getParam('code');
+		$session = Mage::getSingleton('checkout/session');
+
 		if (isset($data)) {
 			$code = $data;
 			try {
 				Mage::getModel('enterprise_giftcardaccount/giftcardaccount')
 					->loadByCode($code)
 					->addToCart();
-				Mage::getSingleton('checkout/session')->addSuccess(
+				$session->addSuccess(
 					$this->__('Gift Card "%s" was added.', Mage::helper('core')->htmlEscape($code))
 				);
 			} catch (Mage_Core_Exception $e) {
 				Mage::dispatchEvent('enterprise_giftcardaccount_add', array('status' => 'fail', 'code' => $code));
 
-				Mage::getSingleton('checkout/session')->addError(
+				$session->addError(
 					$this->__('Coupon code "%s" is not valid.', Mage::helper('core')->htmlEscape($code))
 				);
-				Mage::getSingleton('checkout/session')->addError(
+				$session->addError(
 					$e->getMessage()
 				);
 			} catch (Exception $e) {
-				Mage::getSingleton('checkout/session')->addError(
+				$session->addError(
 					$this->__('Coupon code "%s" is not valid.', Mage::helper('core')->htmlEscape($code))
 				);
-				Mage::getSingleton('checkout/session')->addException($e, $this->__('Cannot apply gift card.'));
+				$session->addException($e, $this->__('Cannot apply gift card.'));
 			}
 		}
 
 
-		$return_url = Mage::getSingleton('checkout/session')->getReturnUrl();
+		$return_url = $session->getReturnUrl();
 
 		if ($return_url) {
+			$session->setKeepOpen(true);
 			$this->getResponse()->setRedirect($return_url);
 		} else {
 			$this->_redirect('checkout/cart');
