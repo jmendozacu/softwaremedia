78be758bfd4429fe766cfbe80b6fa2dcd695d7e0
David Landes <david@davidlandes.net>
2014-06-17 09:32:50 -0600

    Fixed the issues with being sent back to the normal cart when using bad coupon

--- app/code/local/OCM/Checkout/controllers/Checkout/CartController.php
+++ app/code/local/OCM/Checkout/controllers/Checkout/CartController.php
@@ -1,231 +1,222 @@
 <?php
+
 require_once('Mage/Checkout/controllers/CartController.php');
-class OCM_Checkout_Checkout_CartController extends Mage_Checkout_CartController
-{
-    public function couponPostAction(){
-        /**
-         * No reason continue with empty shopping cart
-         */
-        if (!$this->_getCart()->getQuote()->getItemsCount()) {
-            $this->_goBack();
-            return;
-        }
 
-        $couponCode = (string) $this->getRequest()->getParam('coupon_code');
-        if ($this->getRequest()->getParam('remove') == 1) {
-            $couponCode = '';
-        }
-        $oldCouponCode = $this->_getQuote()->getCouponCode();
+class OCM_Checkout_Checkout_CartController extends Mage_Checkout_CartController {
 
-        if (!strlen($couponCode) && !strlen($oldCouponCode)) {
-            $this->_goBack();
-            return;
-        }
+	public function couponPostAction() {
+		/**
+		 * No reason continue with empty shopping cart
+		 */
+		if (!$this->_getCart()->getQuote()->getItemsCount()) {
+			$this->_goBack();
+			return;
+		}
 
-        try {
-            $this->_getQuote()->getShippingAddress()->setCollectShippingRates(true);
-            $this->_getQuote()->setCouponCode(strlen($couponCode) ? $couponCode : '')
-                ->collectTotals()
-                ->save();
+		$couponCode = (string) $this->getRequest()->getParam('coupon_code');
+		if ($this->getRequest()->getParam('remove') == 1) {
+			$couponCode = '';
+		}
+		$oldCouponCode = $this->_getQuote()->getCouponCode();
 
-            if (strlen($couponCode)) {
-                if ($couponCode == $this->_getQuote()->getCouponCode()) {
-                    $this->_getSession()->addSuccess(
-                        $this->__('Coupon code "%s" was applied.', Mage::helper('core')->htmlEscape($couponCode))
-                    );
-                }
-                else {
-                    header('location: ' . Mage::getUrl('giftcard/cart/add',array('code'=>$couponCode)));
-                    exit;
-                }
-            } else {
-                $this->_getSession()->addSuccess($this->__('Coupon code was canceled.'));
-            }
+		if (!strlen($couponCode) && !strlen($oldCouponCode)) {
+			$this->_goBack();
+			return;
+		}
 
-        } catch (Mage_Core_Exception $e) {
-            $this->_getSession()->addError($e->getMessage());
-        } catch (Exception $e) {
-            $this->_getSession()->addError($this->__('Cannot apply the coupon code.'));
-            Mage::logException($e);
-        }
+		try {
+			$this->_getQuote()->getShippingAddress()->setCollectShippingRates(true);
+			$this->_getQuote()->setCouponCode(strlen($couponCode) ? $couponCode : '')
+				->collectTotals()
+				->save();
 
-        $this->_goBack();
-    }
+			if (strlen($couponCode)) {
+				if ($couponCode == $this->_getQuote()->getCouponCode()) {
+					$this->_getSession()->addSuccess(
+						$this->__('Coupon code "%s" was applied.', Mage::helper('core')->htmlEscape($couponCode))
+					);
+				} else {
+					$this->_getSession()->setReturnUrl($this->getRequest()->getParam('return_url'));
+					header('location: ' . Mage::getUrl('giftcard/cart/add', array('code' => $couponCode)));
+					exit;
+				}
+			} else {
+				$this->_getSession()->addSuccess($this->__('Coupon code was canceled.'));
+			}
+		} catch (Mage_Core_Exception $e) {
+			$this->_getSession()->addError($e->getMessage());
+		} catch (Exception $e) {
+			$this->_getSession()->addError($this->__('Cannot apply the coupon code.'));
+			Mage::logException($e);
+		}
 
-    /**
-     * Shopping cart display action
-     */
-    public function newAction()
-    {
-        $cart = $this->_getCart();
-        if ($cart->getQuote()->getItemsCount()) {
-            $cart->init();
-            $cart->save();
+		$this->_goBack();
+	}
 
-            if (!$this->_getQuote()->validateMinimumAmount()) {
-                $minimumAmount = Mage::app()->getLocale()->currency(Mage::app()->getStore()->getCurrentCurrencyCode())
-                    ->toCurrency(Mage::getStoreConfig('sales/minimum_order/amount'));
+	/**
+	 * Shopping cart display action
+	 */
+	public function newAction() {
+		$cart = $this->_getCart();
+		if ($cart->getQuote()->getItemsCount()) {
+			$cart->init();
+			$cart->save();
 
-                $warning = Mage::getStoreConfig('sales/minimum_order/description')
-                    ? Mage::getStoreConfig('sales/minimum_order/description')
-                    : Mage::helper('checkout')->__('Minimum order amount is %s', $minimumAmount);
+			if (!$this->_getQuote()->validateMinimumAmount()) {
+				$minimumAmount = Mage::app()->getLocale()->currency(Mage::app()->getStore()->getCurrentCurrencyCode())
+					->toCurrency(Mage::getStoreConfig('sales/minimum_order/amount'));
 
-                $cart->getCheckoutSession()->addNotice($warning);
-            }
-        }
+				$warning = Mage::getStoreConfig('sales/minimum_order/description') ? Mage::getStoreConfig('sales/minimum_order/description') : Mage::helper('checkout')->__('Minimum order amount is %s', $minimumAmount);
 
-        // Compose array of messages to add
-        $messages = array();
-        foreach ($cart->getQuote()->getMessages() as $message) {
-            if ($message) {
-                // Escape HTML entities in quote message to prevent XSS
-                $message->setCode(Mage::helper('core')->escapeHtml($message->getCode()));
-                $messages[] = $message;
-            }
-        }
-        $cart->getCheckoutSession()->addUniqueMessages($messages);
+				$cart->getCheckoutSession()->addNotice($warning);
+			}
+		}
 
-        /**
-         * if customer enteres shopping cart we should mark quote
-         * as modified bc he can has checkout page in another window.
-         */
-        $this->_getSession()->setCartWasUpdated(true);
+		// Compose array of messages to add
+		$messages = array();
+		foreach ($cart->getQuote()->getMessages() as $message) {
+			if ($message) {
+				// Escape HTML entities in quote message to prevent XSS
+				$message->setCode(Mage::helper('core')->escapeHtml($message->getCode()));
+				$messages[] = $message;
+			}
+		}
+		$cart->getCheckoutSession()->addUniqueMessages($messages);
 
-        Varien_Profiler::start(__METHOD__ . 'cart_display');
-        $this
-            ->loadLayout()
-            ->_initLayoutMessages('checkout/session')
-            ->_initLayoutMessages('catalog/session')
-            ->getLayout()->getBlock('head')->setTitle($this->__('Shopping Cart'));
-        $this->renderLayout();
-        Varien_Profiler::stop(__METHOD__ . 'cart_display');
-    }
+		/**
+		 * if customer enteres shopping cart we should mark quote
+		 * as modified bc he can has checkout page in another window.
+		 */
+		$this->_getSession()->setCartWasUpdated(true);
 
+		Varien_Profiler::start(__METHOD__ . 'cart_display');
+		$this
+			->loadLayout()
+			->_initLayoutMessages('checkout/session')
+			->_initLayoutMessages('catalog/session')
+			->getLayout()->getBlock('head')->setTitle($this->__('Shopping Cart'));
+		$this->renderLayout();
+		Varien_Profiler::stop(__METHOD__ . 'cart_display');
+	}
 
-        /**
-     * Add product to shopping cart action
-     *
-     * @return Mage_Core_Controller_Varien_Action
-     * @throws Exception
-     */
-    public function addAction()
-    {
-        $cart   = $this->_getCart();
-        $params = $this->getRequest()->getParams();
-        try {
-            if (isset($params['qty'])) {
-                $filter = new Zend_Filter_LocalizedToNormalized(
-                    array('locale' => Mage::app()->getLocale()->getLocaleCode())
-                );
-                $params['qty'] = $filter->filter($params['qty']);
-            }
+	/**
+	 * Add product to shopping cart action
+	 *
+	 * @return Mage_Core_Controller_Varien_Action
+	 * @throws Exception
+	 */
+	public function addAction() {
+		$cart = $this->_getCart();
+		$params = $this->getRequest()->getParams();
+		try {
+			if (isset($params['qty'])) {
+				$filter = new Zend_Filter_LocalizedToNormalized(
+					array('locale' => Mage::app()->getLocale()->getLocaleCode())
+				);
+				$params['qty'] = $filter->filter($params['qty']);
+			}
 
-            $product = $this->_initProduct();
-            $related = $this->getRequest()->getParam('related_product');
+			$product = $this->_initProduct();
+			$related = $this->getRequest()->getParam('related_product');
 
-            /**
-             * Check product availability
-             */
-            if (!$product) {
-                $this->_goBack();
-                return;
-            }
-            
-            //SPLIT UP BUNDLE PRODUCTS BEFORE ADDING TO CART
+			/**
+			 * Check product availability
+			 */
+			if (!$product) {
+				$this->_goBack();
+				return;
+			}
+
+			//SPLIT UP BUNDLE PRODUCTS BEFORE ADDING TO CART
 			if (isset($params['bundle_option'])) {
 				$optionCollection = $product->getTypeInstance()->getOptionsCollection();
 				$selectionCollection = $product->getTypeInstance()->getSelectionsCollection($product->getTypeInstance()->getOptionsIds());
 				$options = $optionCollection->appendSelections($selectionCollection);
 				//var_dump($product->getTypeInstance(true)->getOptionsIds($product));
-				foreach( $options as $option )
-				{
+				foreach ($options as $option) {
 					//var_dump($option);
 					//echo $option->getOptionId();
-				    
-				    if (isset($params['bundle_option'][$option->getOptionId()])) {
-				    	$_selections = $option->getSelections();
-					    foreach( $_selections as $selection )
-					    {
-					        if ($selection->getSelectionId() == $params['bundle_option'][$option->getOptionId()]) {
-						    	$bundleProd = Mage::getModel('catalog/product')->load($selection->getProductId());
-						        $cart->addProduct($bundleProd, array('qty' => $params['bundle_option_qty'][$option->getOptionId()]));
+
+					if (isset($params['bundle_option'][$option->getOptionId()])) {
+						$_selections = $option->getSelections();
+						foreach ($_selections as $selection) {
+							if ($selection->getSelectionId() == $params['bundle_option'][$option->getOptionId()]) {
+								$bundleProd = Mage::getModel('catalog/product')->load($selection->getProductId());
+								$cart->addProduct($bundleProd, array('qty' => $params['bundle_option_qty'][$option->getOptionId()]));
 								//exit;
-					        }
-					    }
+							}
+						}
 					}
-				} 
+				}
 			} else {
-		            $cart->addProduct($product, $params);
-		            if (!empty($related)) {
-		                $cart->addProductsByIds(explode(',', $related));
-		            }
+				$cart->addProduct($product, $params);
+				if (!empty($related)) {
+					$cart->addProductsByIds(explode(',', $related));
+				}
 			}
-            $cart->save();
+			$cart->save();
 
-            $this->_getSession()->setCartWasUpdated(true);
+			$this->_getSession()->setCartWasUpdated(true);
 
-            /**
-             * @todo remove wishlist observer processAddToCart
-             */
-            Mage::dispatchEvent('checkout_cart_add_product_complete',
-                array('product' => $product, 'request' => $this->getRequest(), 'response' => $this->getResponse())
-            );
+			/**
+			 * @todo remove wishlist observer processAddToCart
+			 */
+			Mage::dispatchEvent('checkout_cart_add_product_complete', array('product' => $product, 'request' => $this->getRequest(), 'response' => $this->getResponse())
+			);
 
-            if (!$this->_getSession()->getNoCartRedirect(true)) {
-                if (!$cart->getQuote()->getHasError()) {
-                    $message = $this->__('%s was added to your shopping cart.', Mage::helper('core')->escapeHtml($product->getName()));
-                    $this->_getSession()->addSuccess($message);
-                }
-                $this->_goBack();
-            }
-        } catch (Mage_Core_Exception $e) {
-            if ($this->_getSession()->getUseNotice(true)) {
-                $this->_getSession()->addNotice(Mage::helper('core')->escapeHtml($e->getMessage()));
-            } else {
-                $messages = array_unique(explode("\n", $e->getMessage()));
-                foreach ($messages as $message) {
-                    $this->_getSession()->addError(Mage::helper('core')->escapeHtml($message));
-                }
-            }
+			if (!$this->_getSession()->getNoCartRedirect(true)) {
+				if (!$cart->getQuote()->getHasError()) {
+					$message = $this->__('%s was added to your shopping cart.', Mage::helper('core')->escapeHtml($product->getName()));
+					$this->_getSession()->addSuccess($message);
+				}
+				$this->_goBack();
+			}
+		} catch (Mage_Core_Exception $e) {
+			if ($this->_getSession()->getUseNotice(true)) {
+				$this->_getSession()->addNotice(Mage::helper('core')->escapeHtml($e->getMessage()));
+			} else {
+				$messages = array_unique(explode("\n", $e->getMessage()));
+				foreach ($messages as $message) {
+					$this->_getSession()->addError(Mage::helper('core')->escapeHtml($message));
+				}
+			}
 
-            $url = $this->_getSession()->getRedirectUrl(true);
-            if ($url) {
-                $this->getResponse()->setRedirect($url);
-            } else {
-                $this->_redirectReferer(Mage::helper('checkout/cart')->getCartUrl());
-            }
-        } catch (Exception $e) {
-            $this->_getSession()->addException($e, $this->__('Cannot add the item to shopping cart.'));
-            Mage::logException($e);
-            $this->_goBack();
-        }
-    }
-    
-    public function estimatePostAction()
-    {
-        $country    = (string) $this->getRequest()->getParam('country_id');
-        $postcode   = (string) $this->getRequest()->getParam('estimate_postcode');
-        $city       = (string) $this->getRequest()->getParam('estimate_city');
-        $regionId   = (string) $this->getRequest()->getParam('region_id');
-        $region     = (string) $this->getRequest()->getParam('region');
-        $code       = (string) $this->getRequest()->getParam('estimate_method');
+			$url = $this->_getSession()->getRedirectUrl(true);
+			if ($url) {
+				$this->getResponse()->setRedirect($url);
+			} else {
+				$this->_redirectReferer(Mage::helper('checkout/cart')->getCartUrl());
+			}
+		} catch (Exception $e) {
+			$this->_getSession()->addException($e, $this->__('Cannot add the item to shopping cart.'));
+			Mage::logException($e);
+			$this->_goBack();
+		}
+	}
 
-        $this->_getQuote()->getShippingAddress()
-            ->setCountryId($country)
-            ->setCity($city)
-            ->setPostcode($postcode)
-            ->setRegionId($regionId)
-            ->setRegion($region)
-            ->setCollectShippingRates(true);
-        
-        $this->_getQuote()->save();
-        $_SESSION['country'] = $country;
-        if(isset($code) && !empty($code)){
-            header('location: ' . Mage::getUrl('checkout/cart/estimateUpdatePost',array('estimate_method'=>$code)));
-            exit;
-        }
-        $this->_goBack();
-    }
+	public function estimatePostAction() {
+		$country = (string) $this->getRequest()->getParam('country_id');
+		$postcode = (string) $this->getRequest()->getParam('estimate_postcode');
+		$city = (string) $this->getRequest()->getParam('estimate_city');
+		$regionId = (string) $this->getRequest()->getParam('region_id');
+		$region = (string) $this->getRequest()->getParam('region');
+		$code = (string) $this->getRequest()->getParam('estimate_method');
 
+		$this->_getQuote()->getShippingAddress()
+			->setCountryId($country)
+			->setCity($city)
+			->setPostcode($postcode)
+			->setRegionId($regionId)
+			->setRegion($region)
+			->setCollectShippingRates(true);
+
+		$this->_getQuote()->save();
+		$_SESSION['country'] = $country;
+		if (isset($code) && !empty($code)) {
+			header('location: ' . Mage::getUrl('checkout/cart/estimateUpdatePost', array('estimate_method' => $code)));
+			exit;
+		}
+		$this->_goBack();
+	}
 
 }
--- app/code/local/OCM/Checkout/controllers/GiftCart/CartController.php
+++ app/code/local/OCM/Checkout/controllers/GiftCart/CartController.php
@@ -1,35 +1,45 @@
 <?php
-require_once('Enterprise/GiftCardAccount/controllers/CartController.php');
-class OCM_Checkout_GiftCart_CartController extends Enterprise_GiftCardAccount_CartController
-{
-    public function addAction()
-    {
-        $data = Mage::app()->getRequest()->getParam('code');
-        if (isset($data)) {
-            $code = $data;
-            try {
-                Mage::getModel('enterprise_giftcardaccount/giftcardaccount')
-                    ->loadByCode($code)
-                    ->addToCart();
-                Mage::getSingleton('checkout/session')->addSuccess(
-                    $this->__('Gift Card "%s" was added.', Mage::helper('core')->htmlEscape($code))
-                );
-            } catch (Mage_Core_Exception $e) {
-                Mage::dispatchEvent('enterprise_giftcardaccount_add', array('status' => 'fail', 'code' => $code));
 
-                Mage::getSingleton('checkout/session')->addError(
-                    $this->__('Coupon code "%s" is not valid.', Mage::helper('core')->htmlEscape($code))
-                );
-                Mage::getSingleton('checkout/session')->addError(
-                    $e->getMessage()
-                );
-            } catch (Exception $e) {
-                Mage::getSingleton('checkout/session')->addError(
-                    $this->__('Coupon code "%s" is not valid.', Mage::helper('core')->htmlEscape($code))
-                );
-                Mage::getSingleton('checkout/session')->addException($e, $this->__('Cannot apply gift card.'));
-            }
-        }
-        $this->_redirect('checkout/cart');
-    }
+require_once('Enterprise/GiftCardAccount/controllers/CartController.php');
+
+class OCM_Checkout_GiftCart_CartController extends Enterprise_GiftCardAccount_CartController {
+
+	public function addAction() {
+		$data = Mage::app()->getRequest()->getParam('code');
+		if (isset($data)) {
+			$code = $data;
+			try {
+				Mage::getModel('enterprise_giftcardaccount/giftcardaccount')
+					->loadByCode($code)
+					->addToCart();
+				Mage::getSingleton('checkout/session')->addSuccess(
+					$this->__('Gift Card "%s" was added.', Mage::helper('core')->htmlEscape($code))
+				);
+			} catch (Mage_Core_Exception $e) {
+				Mage::dispatchEvent('enterprise_giftcardaccount_add', array('status' => 'fail', 'code' => $code));
+
+				Mage::getSingleton('checkout/session')->addError(
+					$this->__('Coupon code "%s" is not valid.', Mage::helper('core')->htmlEscape($code))
+				);
+				Mage::getSingleton('checkout/session')->addError(
+					$e->getMessage()
+				);
+			} catch (Exception $e) {
+				Mage::getSingleton('checkout/session')->addError(
+					$this->__('Coupon code "%s" is not valid.', Mage::helper('core')->htmlEscape($code))
+				);
+				Mage::getSingleton('checkout/session')->addException($e, $this->__('Cannot apply gift card.'));
+			}
+		}
+
+
+		$return_url = Mage::getSingleton('checkout/session')->getReturnUrl();
+
+		if ($return_url) {
+			$this->getResponse()->setRedirect($return_url);
+		} else {
+			$this->_redirect('checkout/cart');
+		}
+	}
+
 }
