34329360bc0a3225924da184564d1d01e47649e9
Akasirus <jlosee@softwaremedia.com>
2014-07-08 12:44:12 -0600

    add new attribute for updating CPC price

---
+++ app/code/local/OCM/Fulfillment/sql/ocm_fulfillment_setup/mysql4-upgrade-0.1.8-0.1.9.php
@@ -0,0 +1,28 @@
+<?php
+/* @var $installer Mage_Core_Model_Resource_Setup */
+$installer = $this;
+
+$installer->startSetup();
+
+$model=Mage::getModel('eav/entity_setup','core_setup');
+$installer->startSetup();
+$attribute=$model->getAttribute('catalog_product','new_cpc_price');
+$attributeId = $attribute['attribute_id'];
+
+$allAttributeSetIds=$model->getAllAttributeSetIds('catalog_product');
+
+foreach ($allAttributeSetIds as $attributeSetId) {
+try{
+$attributeGroup=$model->getAttributeGroup('catalog_product',$attributeSetId,'Prices');
+$attributeGroupId = $attributeGroup['attribute_group_id'];
+}
+catch(Exception $e)
+{
+$attributeGroup=$model->getDefaultAttributeGroupId('catalog/product',$attributeSetId);
+$attributeGroupId = $attributeGroup['attribute_group_id'];
+}
+$model->addAttributeToSet('catalog_product',$attributeSetId,$attributeGroupId,$attributeId);
+//echo "$model->addAttributeToSet('catalog_product',".$attributeSetId.",".$attributeGroupId.",".$attributeId.")";
+}
+
+$installer->endSetup();
