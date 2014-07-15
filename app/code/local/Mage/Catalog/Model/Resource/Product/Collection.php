3e903dabf3c1153cba4813b4c61cbfacc0d18197
David Landes <david@davidlandes.net>
2014-07-15 16:58:22 -0600

    Added the proper ordering

--- app/code/local/Mage/Catalog/Model/Resource/Product/Collection.php
+++ app/code/local/Mage/Catalog/Model/Resource/Product/Collection.php
@@ -1987,7 +1987,13 @@ class Mage_Catalog_Model_Resource_Product_Collection extends Mage_Catalog_Model_
 				array('bestseller' => 'sales_bestsellers_aggregated_monthly'), 'e.entity_id = bestseller.product_id AND bestseller.store_id = 1 AND bestseller.period = "' . date('Y-m-01', strtotime('first day of previous month')) . '"'
 			);
 
-			$this->getSelect()->order('bestseller.qty_ordered ' . $dir);
+			if ($dir == 'asc') {
+				$dir = 'desc';
+			} else {
+				$dir = 'asc';
+			}
+
+			$this->getSelect()->order('-bestseller.qty_ordered ' . $dir);
 		} else {
 			parent::setOrder($attribute, $dir);
 		}
