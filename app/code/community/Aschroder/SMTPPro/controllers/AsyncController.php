e72da9c814e2628f7be37cfc2afe8dc7c4b8c910
Akasirus <jlosee@softwaremedia.com>
2014-08-28 13:15:30 -0600

    testing

--- app/code/community/Aschroder/SMTPPro/controllers/AsyncController.php
+++ app/code/community/Aschroder/SMTPPro/controllers/AsyncController.php
@@ -3,7 +3,9 @@
 class Aschroder_SMTPPro_AsyncController extends Mage_Core_Controller_Front_Action {
 
 	public function mailAction() {
+		Mage::log('MAIL ACTION ',NULL,'email.log');
 		$queueItemId = $this->getRequest()->getParam('queue_item_id');
+		Mage::log('MAIL ACTION ID ' . $queueItemId,NULL,'email.log');
 		$queueItem = Mage::getModel('smtppro/queue')->load($queueItemId);
 		$params = json_decode($queueItem->getParams(), true);
 
