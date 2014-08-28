9aeadfb3913852e70c40a7cddfc616d9606931ee
Akasirus <jlosee@softwaremedia.com>
2014-08-28 13:29:25 -0600

    testing

--- app/code/community/Aschroder/SMTPPro/controllers/AsyncController.php
+++ app/code/community/Aschroder/SMTPPro/controllers/AsyncController.php
@@ -8,13 +8,16 @@ class Aschroder_SMTPPro_AsyncController extends Mage_Core_Controller_Front_Actio
 		Mage::log('MAIL ACTION ID ' . $queueItemId,NULL,'email.log');
 		$queueItem = Mage::getModel('smtppro/queue')->load($queueItemId);
 		$params = json_decode($queueItem->getParams(), true);
-
+		
+		Mage::log('MAIL Step 2' . $queueItemId,NULL,'email.log');
 		if (Mage::helper('smtppro/mail')->sendMailObject($params['mail_object'], $params['website_model_id'], $params['transport'])) {
 			$queueItem->delete();
+			Mage::log('MAIL Step 3' . $queueItemId,NULL,'email.log');
 		} else {
 			$queueItem->setStatus('failed')->save();
+			Mage::log('MAIL Step 4' . $queueItemId,NULL,'email.log');
 		}
-		
+		Mage::log('MAIL Step 5' . $queueItemId,NULL,'email.log');
 		Mage::log('MAIL ID ' . $queueItemId,NULL,'email.log');
 	}
 
