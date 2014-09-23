41d10199a3b618b231243a64815cc0c3888551aa
Akasirus <jlosee@softwaremedia.com>
2014-09-23 13:16:11 -0600

    don't invoice checkmo payment

--- app/code/local/Mage/Sales/Model/Order/Payment.php
+++ app/code/local/Mage/Sales/Model/Order/Payment.php
@@ -330,7 +330,7 @@ class Mage_Sales_Model_Order_Payment extends Mage_Payment_Model_Info
         $methodInstance->validate();
         $action = $methodInstance->getConfigPaymentAction();
         $sIsAdmin = Mage::getSingleton('core/session')->getSkipKountAdmin();
-        if (!empty($sIsAdmin) && $methodInstance->getCode() != 'purchaseorder' && $methodInstance->getCode() != 'checkmo') {
+        if (!empty($sIsAdmin) && $methodInstance->getCode() != 'purchaseorder') {
 			$action = Mage_Payment_Model_Method_Abstract::ACTION_AUTHORIZE_CAPTURE;
         }
 
@@ -342,11 +342,11 @@ class Mage_Sales_Model_Order_Payment extends Mage_Payment_Model_Info
         }
         
         $sApprove = Mage::getSingleton('core/session')->getKountApprove();
-        if (!empty($sApprove) && $methodInstance->getCode() != 'purchaseorder' && $methodInstance->getCode() != 'checkmo') {
+        if (!empty($sApprove) && $methodInstance->getCode() != 'purchaseorder') {
 			$action = Mage_Payment_Model_Method_Abstract::ACTION_AUTHORIZE_CAPTURE;
         }
         
-        if ($methodInstance->getCode() == 'purchaseorder' || $methodInstance->getCode() == 'checkmo')
+        if ($methodInstance->getCode() == 'purchaseorder')
         	$orderState = Mage_Sales_Model_Order::STATE_PROCESSING;
         	
         if ($action) {
