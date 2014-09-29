68dc9f1b39046e338822a76644b27a8b1a614b79
Akasirus <jlosee@softwaremedia.com>
2014-09-29 15:05:30 -0600

    Update inherit

--- app/code/local/Amasty/Pgrid/Block/Adminhtml/Sales/Order/Grid.php
+++ app/code/local/Amasty/Pgrid/Block/Adminhtml/Sales/Order/Grid.php
@@ -5,7 +5,7 @@
  * @copyright Copyright (c) 2010-2011 Amasty (http://www.amasty.com)
  * @package Amasty_Pgrid
  */
-class Amasty_Pgrid_Block_Adminhtml_Sales_Order_Grid extends Mage_Adminhtml_Block_Catalog_Product_Grid {
+class Amasty_Pgrid_Block_Adminhtml_Sales_Order_Grid extends Mage_Adminhtml_Block_Sales_Order_Grid {
 
 	protected function _prepareColumns() {
 		$parent =  parent::_prepareColumns();
