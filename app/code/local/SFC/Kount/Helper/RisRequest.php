5a9d3063a7f65b4f57992d37d35caeff48090b0e
Akasirus <jlosee@softwaremedia.com>
2014-10-02 14:26:43 -0600

    licensing custom field kount

--- app/code/local/SFC/Kount/Helper/RisRequest.php
+++ app/code/local/SFC/Kount/Helper/RisRequest.php
@@ -308,9 +308,11 @@ class SFC_Kount_Helper_RisRequest extends Mage_Core_Helper_Abstract {
 			$aCart = array();
 			foreach ($oOrder->getAllVisibleItems() as $oItem) {
 				$prod = Mage::getModel('catalog/product')->load($oItem->getProductId(), array('license_nonlicense_dropdown'));
-				$attributeValue = $prod->getResource()->getAttribute('license_nonlicense_dropdown')->getFrontend()->getValue($prod);
-				if ($attributeValue == 'License Product')
-					$hasLicensing = 1;
+				if ($prod->getResource()->getAttribute('license_nonlicense_dropdown')) {
+					$attributeValue = $prod->getResource()->getAttribute('license_nonlicense_dropdown')->getFrontend()->getValue($prod);
+					if ($attributeValue == 'License Product')
+						$hasLicensing = 1;
+				}
 					
 				$aCart[] = new Kount_Ris_Data_CartItem(
 					$oItem->getSku(), $oItem->getName(), ($oItem->getDescription() ? $oItem->getDescription() : ''), round($oItem->getQtyOrdered()), ($baseCurrencyCode == 'USD' ? round($oItem->getBasePrice() * 100) :
