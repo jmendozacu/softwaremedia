5e9986d08e55c978eed04c783dbd10067b32c432
Akasirus <jlosee@softwaremedia.com>
2014-10-31 13:22:16 -0600

    Form updates

--- app/code/local/SoftwareMedia/Customer/controllers/AccountController.php
+++ app/code/local/SoftwareMedia/Customer/controllers/AccountController.php
@@ -119,6 +119,8 @@ class SoftwareMedia_Customer_AccountController extends Mage_Customer_AccountCont
 				$this->_addSessionError($errors);
 			}
 		} catch (Mage_Core_Exception $e) {
+			echo "test 1";
+			die();
 			$session->setCustomerFormData($this->getRequest()->getPost());
 			if ($e->getCode() === Mage_Customer_Model_Customer::EXCEPTION_EMAIL_EXISTS) {
 				$url = $this->_getUrl('customer/account/forgotpassword');
@@ -131,6 +133,8 @@ class SoftwareMedia_Customer_AccountController extends Mage_Customer_AccountCont
 			$session->addError($message);
 			//if (isset($postData['q4_2014_brochure'])) {
 					$errUrl = $this->_getUrl('brochure');
+					echo $errUrl;
+					die();
 					$this->_redirect($errUrl);
 					return;
 			//	}
