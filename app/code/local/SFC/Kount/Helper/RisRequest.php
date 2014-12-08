7d2874078db5f29c6d8973a4b163890786e480cd
Akasirus <jlosee@softwaremedia.com>
2014-12-08 11:05:32 -0700

    Pass in MS License qty to Kount

--- app/code/local/SFC/Kount/Helper/RisRequest.php
+++ app/code/local/SFC/Kount/Helper/RisRequest.php
@@ -313,8 +313,11 @@ class SFC_Kount_Helper_RisRequest extends Mage_Core_Helper_Abstract {
 					$attributeValue = $prod->getResource()->getAttribute('license_nonlicense_dropdown')->getFrontend()->getValue($prod);
 					if ($attributeValue == 'License Product') {
 						$hasLicensing = 1;
-						if (strpos($prod->getProductUrl(),'microsoft'))
+						if (strpos($prod->getProductUrl(),'microsoft')) {
+							Mage::log('MS Licensing',NULL,'kktest.log');
+							die();
 							$msLicensing += $oItem->getQty();
+							}
 					}
 				}
 					
