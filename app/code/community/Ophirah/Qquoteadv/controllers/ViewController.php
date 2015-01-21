0bf5871ddd510d1bf255028208912b81db14c949
Akasirus <jlosee@softwaremedia.com>
2015-01-21 11:42:20 -0700

    remove items before adding quote

--- app/code/community/Ophirah/Qquoteadv/controllers/ViewController.php
+++ app/code/community/Ophirah/Qquoteadv/controllers/ViewController.php
@@ -470,8 +470,10 @@ class Ophirah_Qquoteadv_ViewController extends Mage_Core_Controller_Front_Action
         $_helper = Mage::helper('cataloginventory');
         
         if ($quote = $this->_initQuote()) {
+        	Mage::helper('qquoteadv')->setActiveConfirmMode(true); 
 			Mage::getSingleton('checkout/session')->clear();
 			Mage::getSingleton('checkout/cart')->truncate();
+			Mage::getSingleton('checkout/cart')->getQuote()->delete();
             $quoteId    = (int) $this->getRequest()->getParam('id');
             $params     = $this->getRequest()->getParams();
 			
