8ff94d23ba73c4ac1e1fb93438dde499882ba049
Akasirus <jlosee@softwaremedia.com>
2015-01-28 11:43:39 -0700

    Update chat param

--- app/code/local/SoftwareMedia/Ratings/Block/Rating/List.php
+++ app/code/local/SoftwareMedia/Ratings/Block/Rating/List.php
@@ -54,8 +54,9 @@ class SoftwareMedia_Ratings_Block_Rating_List extends Mage_Core_Block_Template
     
     public function getRatingURL($rating) {
 	    
-	    if (Mage::app()->getRequest()->getParam('user'))
+	    if (Mage::app()->getRequest()->getParam('chat'))
 	    	$chat = '/chat/1/';
+	    	
 	    return "/smratings/rating/rate/user_id/" . Mage::app()->getRequest()->getParam('user') . "/rating/" . $rating . $chat;
     }
     
