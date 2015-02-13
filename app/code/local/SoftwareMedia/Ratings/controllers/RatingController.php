efb92e6eb33becf9870b13b7c21a18ffcfe48bdd
Akasirus <jlosee@softwaremedia.com>
2015-02-13 13:47:01 -0700

    IP Logging

--- app/code/local/SoftwareMedia/Ratings/controllers/RatingController.php
+++ app/code/local/SoftwareMedia/Ratings/controllers/RatingController.php
@@ -58,10 +58,7 @@ class SoftwareMedia_Ratings_RatingController extends Mage_Core_Controller_Front_
 		    $rating->setCustomerId(Mage::getSingleton('customer/session')->getId());
 		}
             
-		if (Mage::app()->getRequest()->getServer('HTTP_X_FORWARDED_FOR'))
-			$rating->setIp(Mage::app()->getRequest()->getServer('HTTP_X_FORWARDED_FOR'));
-		 else
-		 	$rating->setIp(Mage::helper('core/http')->getRemoteAddr());
+		 $rating->setIp($this->get_ip_address());
 	     
 	     Mage::log("Remote Addr: " . Mage::helper('core/http')->getRemoteAddr(), NULL,'addr.log');
 	     Mage::log("FORWARDED Addr: " . Mage::app()->getRequest()->getServer('HTTP_X_FORWARDED_FOR'), NULL,'addr.log');
@@ -72,6 +69,52 @@ class SoftwareMedia_Ratings_RatingController extends Mage_Core_Controller_Front_
 	     $this->_redirect('*/*/index/rating_id/' . $rating->getId());
     }
      
+      public function get_ip_address() {
+		  // Check for shared internet/ISP IP
+		  if (!empty($_SERVER['HTTP_CLIENT_IP']) && $this->validate_ip($_SERVER['HTTP_CLIENT_IP']))
+		   return $_SERVER['HTTP_CLIENT_IP'];
+		
+		  // Check for IPs passing through proxies
+		  if (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+		   // Check if multiple IP addresses exist in var
+		    $iplist = explode(',', $_SERVER['HTTP_X_FORWARDED_FOR']);
+		    foreach ($iplist as $ip) {
+		     if ($this->validate_ip($ip))
+		      return $ip;
+		    }
+		   }
+		  }
+		  if (!empty($_SERVER['HTTP_X_FORWARDED']) && $this->validate_ip($_SERVER['HTTP_X_FORWARDED']))
+		   return $_SERVER['HTTP_X_FORWARDED'];
+		  if (!empty($_SERVER['HTTP_X_CLUSTER_CLIENT_IP']) && $this->validate_ip($_SERVER['HTTP_X_CLUSTER_CLIENT_IP']))
+		   return $_SERVER['HTTP_X_CLUSTER_CLIENT_IP'];
+		  if (!empty($_SERVER['HTTP_FORWARDED_FOR']) && $this->validate_ip($_SERVER['HTTP_FORWARDED_FOR']))
+		   return $_SERVER['HTTP_FORWARDED_FOR'];
+		  if (!empty($_SERVER['HTTP_FORWARDED']) && $this->validate_ip($_SERVER['HTTP_FORWARDED']))
+		   return $_SERVER['HTTP_FORWARDED'];
+		
+		  // Return unreliable IP address since all else failed
+		  return $_SERVER['REMOTE_ADDR'];
+		 }
+		
+		 /**
+		  * Ensures an IP address is both a valid IP address and does not fall within
+		  * a private network range.
+		  *
+		  * @access public
+		  * @param string $ip
+		  */
+		 public function validate_ip($ip) {
+		     if (filter_var($ip, FILTER_VALIDATE_IP, 
+		                         FILTER_FLAG_IPV4 | 
+		                         FILTER_FLAG_IPV6 |
+		                         FILTER_FLAG_NO_PRIV_RANGE | 
+		                         FILTER_FLAG_NO_RES_RANGE) === false)
+		         return false;
+		     self::$ip = $ip;
+		     return true;
+		 }
+		 
     public function indexAction()
     {
         $this->loadLayout();
