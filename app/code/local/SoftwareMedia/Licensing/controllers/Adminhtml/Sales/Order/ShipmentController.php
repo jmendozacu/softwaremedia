d92344d9086d4bf52e500e7cf4a184c2c00737d0
Akasirus <jlosee@softwaremedia.com>
2015-04-02 10:08:37 -0600

    license shipment controller

--- app/code/local/SoftwareMedia/Licensing/controllers/Adminhtml/Sales/Order/ShipmentController.php
+++ app/code/local/SoftwareMedia/Licensing/controllers/Adminhtml/Sales/Order/ShipmentController.php
@@ -153,8 +153,10 @@ class SoftwareMedia_Licensing_Adminhtml_Sales_Order_ShipmentController extends M
 	        $template->setSenderName('Software Media Licensing');
 	        $template->setSenderEmail('licensing@softwaremedia.com');
 	        $template->setTemplateSubject($vars['subject']);
-	        $template->addBcc("licensing@softwaremedia.com");
-	        $template->send($email, $email, $vars);
+	        //$template->addBcc("licensing@softwaremedia.com");
+	        echo $_SERVER['HTTP_HOST'];
+	        die();
+	        $template->send('jlosee@softwaremedia.com', $email, $vars);
         
 		}
 	}
