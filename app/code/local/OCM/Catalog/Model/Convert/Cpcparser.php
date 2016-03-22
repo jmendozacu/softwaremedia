248c3f98736d92a9ccddfeaf926563c01a1f945a
Jeff Losee <jlosee@softwaremedia.com>
2016-03-22 19:51:03 +0000

    committing file to test decimal place fix that could only be tested on live server

--- app/code/local/OCM/Catalog/Model/Convert/Cpcparser.php
+++ app/code/local/OCM/Catalog/Model/Convert/Cpcparser.php
@@ -548,7 +548,7 @@ class OCM_Catalog_Model_Convert_Cpcparser
 			$row['description_stripped'] = substr($row['description_stripped'],0,990);
 			
 			if (!$row['cpc_price'])
-				$row['cpc_price'] = $row['price'];
+				$row['cpc_price'] = Mage::helper('core')->currency($row['price'], true, false);
 				
 			$params = array('_ignore_category'=>true);
 
